da32c28956add7756e09a485cd06942beb53988c
WuNL <wunailiang@gmail.com>
2017-10-20 17:18:05 +0800

    update setting

--- mcuWeb/system/forms.py
+++ mcuWeb/system/forms.py
@@ -7,9 +7,9 @@ username_style = {'class': 'required'}
 password_style = {'class': 'required'}
 
 class SigninFormExtra(AuthenticationForm):
-	identification = forms.CharField(label="userName by WuNL",
+	identification = forms.CharField(label=_("Email or username"),
                            widget=forms.TextInput(attrs=username_style),
                            max_length=75,
-                           error_messages={'required': "Either supply us with your email or username."})
+                           error_messages={'required': _("Either supply us with your email or username.")})
 	password = forms.CharField(label=_("Password"),widget=forms.PasswordInput(attrs=password_style, render_value=False))
 	remember_me = forms.BooleanField(widget=forms.CheckboxInput(),required=False,label=_('Remember me for %(days)s') % {'days': 30} )
