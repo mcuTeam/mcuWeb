9046f2ef21d245f51d26497cae2f453c94aa9f64
WuNL <wunailiang@gmail.com>
2017-10-20 08:55:11 +0800

    update celery; create schedule

--- mcuWeb/system/urls.py
+++ mcuWeb/system/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from system import views as system_view
 
 urlpatterns = [
-    url(r'^home/', system_view.homeView),
-    url(r'^', system_view.homeView),
+    url(r'^home/$', system_view.homeView),
+    url(r'^$', system_view.homeView),
 ]
