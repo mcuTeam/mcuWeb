ff472240ef5dcc0b9cd8a9289859691abf9c5b0f
WuNL <wunailiang@gmail.com>
2017-11-16 16:57:15 +0800

    add meeting template

--- mcuWeb/system/models.py
+++ mcuWeb/system/models.py
@@ -8,4 +8,5 @@ class terminal(models.Model):
 	terminalIP = models.GenericIPAddressField(unique=True,max_length=200,blank=False,null=False,verbose_name=u"IP",error_messages={'unique':u"重复IP,请重新输入!"})
 
 class meetingTemplate(models.Model):
-	name = models.CharField(max_length=200,blank=False,null=False,verbose_name=u"meetingTemplate名称")
+	name = models.CharField(unique=True,max_length=200,blank=False,null=False,verbose_name=u"meetingTemplate名称")
+	bandwidth = models.CharField(max_length=200,blank=False,null=False,default="4M",verbose_name=u"bandwidth")
